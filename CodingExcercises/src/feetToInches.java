3d121a80943190b9ae7dd79629ab26caa0cec0f2
Kanchan Chaudhary <40591211+kanchan7c@users.noreply.github.com>
2021-04-20 15:20:45 +0530

    Added Bytes Converter

--- CodingExcercises/src/feetToInches.java
+++ CodingExcercises/src/feetToInches.java
@@ -1,12 +1,12 @@
 public class feetToInches {
-    public static double calcFeetAndInchesToCentimeters(int feet, int inches){
-        if (feet >= 0 && inches >0 && inches <=12){
-            return ((inches + (feet * 12)) * 2.54);
-        }else {
+    public static double calcFeetAndInchesToCentimeters(double feet, double inches){
+        if ((feet < 0 )|| ((inches < 0) || (inches > 12))){
             return -1;
+        }else {
+            return ((inches + (feet * 12)) * 2.54);
         }
     }
     public static void main(String[] args) {
-        System.out.println(calcFeetAndInchesToCentimeters(10, 8) + " cm");
+        System.out.println(calcFeetAndInchesToCentimeters(6, 0) + " cm");
     }
 }
