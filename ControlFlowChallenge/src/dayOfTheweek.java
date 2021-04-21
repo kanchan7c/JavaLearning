092fb5b42411b1969f8e30e81cfb6564c57f6bc9
Kanchan Chaudhary <40591211+kanchan7c@users.noreply.github.com>
2021-04-21 12:40:08 +0530

    Day of the week challenge

---
+++ ControlFlowChallenge/src/dayOfTheweek.java
@@ -0,0 +1,26 @@
+public class dayOfTheweek {
+    public static void printDayOfTheWeek(int day){
+        switch (day){
+            case 1:
+                System.out.println("Day of the Week is Sunday"); break;
+            case 2:
+                System.out.println("Day of the Week is Monday"); break;
+            case 3:
+                System.out.println("Day of the Week is Tuesday"); break;
+            case 4:
+                System.out.println("Day of the Week is Wednesday"); break;
+            case 5:
+                System.out.println("Day of the Week is Thursday"); break;
+            case 6:
+                System.out.println("Day of the Week is Friday"); break;
+            case 7:
+                System.out.println("Day of the Week is Saturday"); break;
+            default:
+                System.out.println("Invalid value! Enter a number between 1 - 7"); break;
+        }
+    }
+
+    public static void main(String[] args) {
+        printDayOfTheWeek(10);
+    }
+}
